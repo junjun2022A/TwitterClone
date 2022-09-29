8e3216051967f0f2e7ab5cc90afc3d983e746aba
junjun2022A <jsaito005@gmail.com>
2022-09-30 04:33:10 +0900

    ログイン機能

--- util.php
+++ util.php
@@ -20,15 +20,12 @@ function buildImagePath(string $name = null, string $type)
     return HOME_URL . 'Views/img_uploaded/' . $type . '/' . htmlspecialchars($name);
 }
 
-
-
 /**
  * 指定した日時からどれだけ経過したかを取得
  * 
  * @param string $datetime 日時
  * @return string
  */
-
 function convertToDayTimeAgo(string $datetime)
 {
     $unix = strtotime($datetime);
@@ -46,7 +43,7 @@ function convertToDayTimeAgo(string $datetime)
         $unit = '時間前';
     } elseif ($diff_sec < 2764800) {
         $time = $diff_sec / 86400;
-        $unit = '日前'; 
+        $unit = '日前';
     } else {
 
         if (date('Y') !== date('Y', $unix)) {
@@ -58,4 +55,66 @@ function convertToDayTimeAgo(string $datetime)
     }
 
     return (int)$time . $unit;
+}
+/**
+    * ユーザー情報をセッションに保存
+    *
+    * @param array $user
+    * @return void
+    */
+function saveUserSession(array $user)
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+ 
+    $_SESSION['USER'] = $user;
+}
+ 
+/**
+    * ユーザー情報をセッションから削除
+    *
+    * @return void
+    */
+function deleteUserSession()
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+ 
+    // セッションのユーザー情報を削除
+    unset($_SESSION['USER']);
+}
+
+/**
+ * セッションのユーザー情報を取得
+ * 
+ * @return array|false
+ */
+function getUserSession()
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+
+    if(!isset($_SESSION['USER'])){
+        // セッションにユーザー情報がない
+        return false;
+    }
+
+    $user = $_SESSION['USER'];
+
+    //画像のファイル名からファイルのURLを取得
+    if (!isset($user['image_name'])) {
+        $user['image_name'] = null;
+    }
+    $user['image_path'] = buildImagePath($user['image_name'], 'user');
+
+    return $user;
 }
